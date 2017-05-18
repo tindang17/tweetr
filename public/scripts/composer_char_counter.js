8f74a8b51a29f50ef00d9981143e8a0a6a705a48
Tin Trung Dang <tindang1710@gmail.com>
2017-05-18 01:03:21 +0000

    created a function that shows user how many more characters they can type

---
+++ public/scripts/composer_char_counter.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.container .new-tweet textarea').keyup(function(){
+    const inputLength = $(this).val().length;
+    const $counter = $('.new-tweet .counter').text(140 - inputLength);
+    if(inputLength <= 140) {
+      $counter.css('color', 'black');
+    } else {
+      $counter.css('color', 'red');
+    }
+  });
+});
