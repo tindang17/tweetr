4bbf5216cf524dde2e5742edb3b0c206dac4f13a
Tin Trung Dang <tindang1710@gmail.com>
2017-05-19 20:40:38 +0000

    delete the hardcoded data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  var tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-    // function that render tweet to createTweetElement
 
+  // function that render tweet to createTweetElement
   function renderTweets(tweets){
     $("#tweet-display").empty();
     $.each(tweets, function(index, tweet) {
@@ -92,12 +46,10 @@ $(document).ready(function() {
   }
 
 
-
-
   var $form = $("form");
   var $contentInput = $form.find("textarea[name=text]");
-  console.log($contentInput);
   $form.submit(function(evt) {
+// Prevent browser from redirecting when user click submit button
     evt.preventDefault();
     var content = $contentInput.val();
 // form validation
@@ -111,7 +63,6 @@ $(document).ready(function() {
   });
 
 
-// Prevent browser from redirecting when user click submit button
     // Ajax post request
 
     function createNewTweet() {
@@ -126,8 +77,8 @@ $(document).ready(function() {
       $.ajax({
         url: "/tweets",
         method: "GET"
-      }).done(function(tweets) {
-        renderTweets(tweets);
+      }).done(function(data) {
+        renderTweets(data);
       })
     }
   loadTweets();
