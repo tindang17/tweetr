525b8caebf3f784671b4295c426e1a8d77e9619a
Tin Trung Dang <tindang1710@gmail.com>
2017-11-21 01:11:02 -0800

    added process.env.PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 "use strict";
 require('dotenv').config();
 // Basic express setup:
-const PORT          = 8080;
+const PORT          = process.env.PORT||8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
